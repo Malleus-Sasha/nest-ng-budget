b5ad5b8385aa65b7c2563ced72b0d54391b8ff01
Alex Maslk <amksandr@gmail.com>
2024-01-17 15:38:57 +0200

    NG: Categories form

--- client/src/app/components/categories/categories.component.ts
+++ client/src/app/components/categories/categories.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { faEdit, faRemove } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-categories',
-  template: `
-    <p>
-      categories works!
-    </p>
-  `,
+  templateUrl: './categories.component.html',
   styles: ``
 })
 export class CategoriesComponent {
+  categoryForm: FormGroup;
+  removeIcon = faRemove;
+  editIcon = faEdit;
 
+  constructor() {
+    this.categoryForm = new FormGroup({
+      title: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    })
+  }
+
+  onSubmit() {
+    console.log('ctgr sbmt');
+    if (this.categoryForm.value) {
+      console.log('Ctgrs:', this.categoryForm.value);
+    }
+  }
 }
